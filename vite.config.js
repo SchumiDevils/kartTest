3bb2b411feccb153ef06666865c73d8fada9b551
Iachim Cristian <2238224@student.uma.pt>
2026-02-03 00:29:25 +0100

    Initial commit: Kart Controller with BLE

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/kart-controller/', // Change this to match your GitHub repo name
 })
